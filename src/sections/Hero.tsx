b7d23847a176e64976ec867d65714f55ac261a4b
Arjun Pandit <arjunpanditmjpru@gmail.com>
2026-07-09 15:12:40 +0530

    Update sections

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -54,7 +54,7 @@ const Hero = () => {
                         </a>
 
                         <a 
-                            href='/arjun_upd_resume.pdf'
+                            href='/arjun_resume.pdf'
                             target='_blank'
                             rel="noopener noreferrer"
                             className="flex items-center gap-2 text-blue-400 bg-transparent border border-blue-500/50 hover:bg-blue-500/20 hover:shadow-[0_0_6px_rgba(59,130,246,0.5)] border-blue-400 duration-300 px-6 py-2 rounded transition-all"
